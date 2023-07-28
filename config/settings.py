520c1f2467f4ed5f5dca4c97916a3eae7b16cf4b
soqula <fsd1015@gmail.com>
2023-07-28 18:26:15 +0900

    ALLOW_HOSTSをenvファイルから読み込み

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = ["localhost","10.0.2.2","127.0.0.1"]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
